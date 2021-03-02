bd8d8ad4e115b7e00a5b879502b2942f2663b3b6
Nipers <48409728+Nipers@users.noreply.github.com>
2021-03-02 20:02:59 +0800

    解出来了

--- 883问题.cpp
+++ 883问题.cpp
@@ -1,11 +1,12 @@
+  
 #include<iostream>
 using namespace std;
-int table[7][1000] = {{}}, choice[1000] = {}, step = 1;
+int table[7][1000] = {{}}, choice[1000] = {}, step = 1, fail[7][1000] = {{}}, size = 0;
 bool move(int c) {//该选择能不能走
     int chosen[7] = {};//是否有改动，无改动则继承上一步的值
     switch (c) {
         case 1:// 0 -> 1
-            if (table[1][step - 1] == 8 || table[0][step - 1] == 8 || table[0][step - 1] == 0)
+            if (table[1][step - 1] == 8 || table[0][step - 1] == 8 || table[0][step - 1] == 0|| table[1][step - 1] == 0)
                 return false;
             chosen[0] = chosen[1] = 1;
             table[1][step] = table[0][step - 1] + table[1][step - 1];
@@ -29,7 +30,7 @@ bool move(int c) {//该选择能不能走
                 table[0][step] = 0;
             break;
         case 3:// 1 -> 0
-            if (table[0][step - 1] == 8 || table[1][step - 1] == 8 || table[1][step - 1] == 0)
+            if (table[0][step - 1] == 8 || table[1][step - 1] == 8 || table[1][step - 1] == 0|| table[0][step - 1] == 0)
                 return false;
             chosen[0] = chosen[1] = 1;
             table[0][step] = table[0][step - 1] + table[1][step - 1];
@@ -164,15 +165,21 @@ bool move(int c) {//该选择能不能走
             return false;
             break;
     }
-    for (int i = 0; i < step; i++) {
-        if (table[0][i] == table[step][0] && table[1][i] == table[step][1] && table[2][i] == table[step][2]
-         && table[3][i] == table[step][3] && table[4][i] == table[step][4]
-         && table[5][i] == table[step][5] && table[6][i] == table[step][6])
-            return false;
-    }
     for (int i = 0; i < 7; i++) {
         if (!chosen[i])
             table[i][step] = table[i][step - 1];
+    }
+    for (int i = 0; i < step; i++) {
+        if (table[0][i] == table[0][step] && table[1][i] == table[1][step] && table[2][i] == table[2][step]
+         && table[3][i] == table[3][step] && table[4][i] == table[4][step]
+         && table[5][i] == table[5][step] && table[6][i] == table[6][step])
+            return false;
+    }
+    for (int i = 0; i < size; i++) {
+        if (fail[0][i] == table[0][step] && fail[1][i] == table[1][step] && fail[2][i] == table[2][step]
+         && fail[3][i] == table[3][step] && fail[4][i] == table[4][step]
+         && fail[5][i] == table[5][step] && fail[6][i] == table[6][step])
+            return false;
     }
     return true;
 }
@@ -184,7 +191,6 @@ int main() {
         cout << "old choice " << choice[step] << endl;
         while (choice[step] < 18) {
             choice[step]++;
-            cout << "try choice " << choice[step] << endl;
             if (move(choice[step])) {
                 cout << "right choice " << choice[step] << endl << "result: ";
                 for (int i = 0; i < 7; i++) {
@@ -201,6 +207,26 @@ int main() {
         if (end)
             break;
         if (choice[step] >= 18) {
+            bool same, need = true;
+            for (int i = 0; i < size; i++) {
+                same = true;
+                for (int j = 0; j < 7; j++) {
+                    if (table[j][step] != fail[j][i])
+                        same = false;
+                }
+                if (same) {
+                    need = false;
+                }
+            }
+            if (need) {
+                cout << "fail case: ";
+                for (int j = 0; j < 7; j++) {
+                    fail[j][size] = table[j][step];
+                    cout << table[j][step] << " ";
+                }
+                cout << endl;
+                size++;
+            }
             while (choice[step] >= 18) {
                 choice[step] = 0;
                 step--;
@@ -212,3 +238,29 @@ int main() {
     }
     return 0;
 }
+/* 
+8 8 0 0 0 0 0 
+5 8 3 0 0 0 0 
+5 8 0 3 0 0 0
+2 8 3 3 0 0 0
+0 8 3 3 2 0 0
+3 8 0 3 2 0 0
+3 5 3 3 2 0 0
+6 5 0 3 2 0 0
+6 2 3 3 2 0 0
+8 2 1 3 2 0 0
+8 2 0 4 2 0 0
+8 0 2 4 2 0 0
+7 0 3 4 2 0 0
+7 3 0 4 2 0 0
+4 3 3 4 2 0 0
+4 6 0 4 2 0 0
+1 6 3 4 2 0 0
+1 8 1 4 2 0 0
+1 8 0 4 2 1 0
+1 5 3 4 2 1 0
+4 5 0 4 2 1 0
+4 2 3 4 2 1 0
+7 2 0 4 2 1 0
+7 0 0 4 2 1 0
+4 0 3 4 4 1 0*/
