ee4fa301ecc02c9704d0c7b9056c7140b83b9074
Nipers <48409728+Nipers@users.noreply.github.com>
2021-03-02 09:49:58 +0800

    Update 883问题.cpp

--- 深搜/883问题.cpp
+++ 深搜/883问题.cpp
@@ -1,82 +1,206 @@
 #include<iostream>
 using namespace std;
 int table[7][1000] = {{}}, choice[1000] = {}, step = 1;
-bool move(int choice) {//该选择能不能走
-    int chosen[7] = {};
-    switch (choice) {
-        case 1://x -> y
+bool move(int c) {//该选择能不能走
+    int chosen[7] = {};//是否有改动，无改动则继承上一步的值
+    switch (c) {
+        case 1:// 0 -> 1
+            if (choice[step - 1] == 3 || table[1][step - 1] == 8 || table[0][step - 1] == 8 || table[0][step - 1] == 0)
+                return false;
             chosen[0] = chosen[1] = 1;
+            table[1][step] = table[0][step - 1] + table[1][step - 1];
+            if (table[1][step] > 8) {
+                table[0][step] = table[1][step] - 8;
+                table[1][step] = 8;
+            }
+            else 
+                table[0][step] = 0;
             break;
-        case 2:// x -> z
+        case 2:// 0 -> 2
+            if (choice[step - 1] == 5 || table[0][step - 1] == 0 || table[2][step - 1] >= 3)
+                return false;
             chosen[0] = chosen[2] = 1;
+            table[2][step] = table[0][step - 1] + table[2][step - 1];
+            if (table[2][step] > 3) {
+                table[0][step] = table[2][step] - 3;
+                table[2][step] = 3;
+            }
+            else 
+                table[0][step] = 0;
             break;
-        case 3:// y -> x
+        case 3:// 1 -> 0
+            if (table[0][step - 1] == 8 || choice[step - 1] == 1 || table[1][step - 1] == 8 || table[1][step - 1] == 0)
+                return false;
             chosen[0] = chosen[1] = 1;
+            table[0][step] = table[0][step - 1] + table[1][step - 1];
+            if (table[0][step] > 8) {
+                table[1][step] = table[0][step] - 8;
+                table[0][step] = 8;
+            }
+            else 
+                table[1][step] = 0;
             break;
-        case 4:// y -> z
+        case 4:// 1 -> 2
+            if (choice[step - 1] == 6 || table[1][step - 1] == 0 || table[2][step - 1] >= 3)
+                return false;
             chosen[2] = chosen[1] = 1;
+            table[2][step] = table[1][step - 1] + table[2][step - 1];
+            if (table[2][step] > 3) {
+                table[1][step] = table[2][step] - 3;
+                table[2][step] = 3;
+            }
+            else 
+                table[1][step] = 0;
             break;
-        case 5:// z -> x
+        case 5:// 2 -> 0
+            if (choice[step - 1] == 2 || table[2][step - 1] == 0 || table[0][step - 1] >= 8)
+                return false;
             chosen[0] = chosen[2] = 1;
+            table[0][step] = table[0][step - 1] + table[2][step - 1];
+            if (table[0][step] > 8) {
+                table[2][step] = table[0][step] - 8;
+                table[0][step] = 8;
+            }
+            else 
+                table[2][step] = 0;
             break;
-        case 6: // z -> y
+        case 6: // 2 -> 1
+            if (choice[step - 1] == 4 || table[2][step - 1] == 0 || table[1][step - 1] >= 8)
+                return false;
             chosen[2] = chosen[1] = 1;
+            table[1][step] = table[1][step - 1] + table[2][step - 1];
+            if (table[1][step] > 8) {
+                table[2][step] = table[1][step] - 8;
+                table[1][step] = 8;
+            }
+            else 
+                table[2][step] = 0;
             break;
-        case 7: // x -> w
+        case 7: // 0 -> 3
+            if (table[0][step - 1] + table[3][step - 1] > 4 || table[0][step - 1] == 0)
+                return false;
             chosen[0] = chosen[3] = 1;
-            
+            table[3][step] = table[0][step - 1] + table[3][step - 1];
+            table[0][step] = 0;                                    
             break;
-        case 8:// x -> o
+        case 8:// 0 -> 4
+            if (table[0][step - 1] + table[4][step - 1] > 4 || table[0][step - 1] == 0)
+                return false;
             chosen[0] = chosen[4] = 1;
+            table[4][step] = table[0][step - 1] + table[4][step - 1];
+            table[0][step] = 0;                                    
             break;
-        case 9:// x -> p
+        case 9:// 0 -> 5
+            if (table[0][step - 1] + table[5][step - 1] > 4 || table[0][step - 1] == 0)
+                return false;
             chosen[0] = chosen[5] = 1;
+            table[5][step] = table[0][step - 1] + table[5][step - 1];
+            table[0][step] = 0;                                    
             break;
-        case 10:// x -> q
+        case 10:// 0 -> 6
+            if (table[0][step - 1] + table[6][step - 1] > 4 || table[0][step - 1] == 0)
+                return false;
             chosen[0] = chosen[6] = 1;
-            
+            table[6][step] = table[0][step - 1] + table[6][step - 1];  
+            table[0][step] = 0;                                              
             break;
-        case 11:// y -> w
+        case 11:// 1 -> 3
+            if (table[1][step - 1] + table[3][step - 1] > 4 || table[1][step - 1] == 0)
+                return false;
             chosen[1] = chosen[3] = 1;
+            table[3][step] = table[1][step - 1] + table[3][step - 1];
+            table[1][step] = 0;                                    
             break;
-        case 12:// y -> o
+        case 12:// 1 -> 4
+            if (table[1][step - 1] + table[4][step - 1] > 4 || table[1][step - 1] == 0)
+                return false;
             chosen[1] = chosen[4] = 1;
+            table[4][step] = table[1][step - 1] + table[4][step - 1];
+            table[1][step] = 0;                                    
             break;
-        case 13:// y -> p
+        case 13:// 1 -> 5
+            if (table[1][step - 1] + table[5][step - 1] > 4 || table[1][step - 1] == 0)
+                return false;
             chosen[1] = chosen[5] = 1;
-            
+            table[5][step] = table[1][step - 1] + table[5][step - 1]; 
+            table[1][step] = 0;                                               
             break;
-        case 14:// y -> q
+        case 14:// 1 -> 6
+            if (table[1][step - 1] + table[6][step - 1] > 4 || table[1][step - 1] == 0)
+                return false;
             chosen[1] = chosen[6] = 1;
-            
+            table[6][step] = table[1][step - 1] + table[6][step - 1];  
+            table[1][step] = 0;                                    
             break;
-        case 15:// z -> w
-            chosen[2] = chosen[2] = 1;
-            
+        case 15:// 2 -> 3
+            if (table[2][step - 1] + table[3][step - 1] > 4 || table[2][step - 1] == 0)
+                return false;
+            chosen[2] = chosen[3] = 1;
+            table[3][step] = table[2][step - 1] + table[3][step - 1];  
+            table[2][step] = 0;                      
             break;
-        case 16:// z -> o
+        case 16:// 2 -> 4
+            if (table[2][step - 1] + table[4][step - 1] > 4 || table[2][step - 1] == 0)
+                return false;
             chosen[2] = chosen[4] = 1;
-            
+            table[4][step] = table[2][step - 1] + table[4][step - 1];  
+            table[2][step] = 0;                      
             break;
-        case 17:// z -> p
+        case 17:// 2 -> 5
+            if (table[2][step - 1] + table[5][step - 1] > 4 || table[2][step - 1] == 0)
+                return false;
             chosen[2] = chosen[5] = 1;
-            
+            table[5][step] = table[2][step - 1] + table[5][step - 1];  
+            table[2][step] = 0;                         
             break;
-        case 18:// z -> q
+        case 18:// 2 -> 6
+            if (table[2][step - 1] + table[6][step - 1] > 4 || table[2][step - 1] == 0)
+                return false;
             chosen[2] = chosen[6] = 1;
-            
+            table[6][step] = table[2][step - 1] + table[6][step - 1];  
+            table[2][step] = 0;            
             break;      
         default:
+            return false;
             break;
     }
     for (int i = 0; i < 7; i++) {
         if (!chosen[i])
-            table[i][step] = table[i][step];
+            table[i][step] = table[i][step - 1];
     }
     return true;
 }
 int main() {
-    
+    table[0][0] = table[1][0] = 8; 
+    bool end = false;
+    while (!end) {
+        cout << "step" << step << " :" << endl;
+        cout << "old choice " << choice[step] << endl;
+        while (choice[step] < 18) {
+            choice[step]++;
+            cout << "try choice " << choice[step] << endl;
+            if (move(choice[step])) {
+                cout << "right choice " << choice[step] << endl << "result: ";
+                for (int i = 0; i < 7; i++) {
+                    cout << table[i][step] << " ";
+                }
+                cout << endl;
+                step++;
+                if (table[3][step] == 4 && table[4][step] == 4 && table[5][step] == 4 && table[6][step] == 4)
+                    end = true;
+                break;
+            }
+        }
+        cout << endl;
+        if (end)
+            break;
+        if (choice[step] >= 18) {
+            while (choice[step] >= 18) {
+                choice[step] = 0;
+                step--;
+            }
+        }
+    }
     for (int i = 0; i < step; i++) {
         cout << table[0][i] << " " << table[1][i] << " " << table[2][i] << " " << table[3][i] << " " << table[4][i] << " " << table[5][i] << " " << table[6][i] << endl; 
     }
